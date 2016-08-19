b03211d98975ebe3842cc6cb60bf8576cd5dd71b
alessio <asoseil@gmail.com>
2016-08-19 23:27:20 +0200

    echo framework example

---
+++ echo/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Watch on 0.0.0.0:3000")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World, Echo Framework!")
+	})
+	e.Run(standard.New("0.0.0.0:3000"))
+	for{
+		fmt.Println("test")
+	}
+}
