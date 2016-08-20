b63c006ff14396a992d54f51f8f556f9e39a796b
alessio <asoseil@gmail.com>
2016-08-20 13:18:50 +0200

    nested directories for print project

---
+++ print/printer/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main(){
+	fmt.Println("Start")
+	for i := 0; i < 10; i++ {
+		fmt.Println("Tick",i)
+		time.Sleep(2 * time.Second)
+	}
+	fmt.Println("End")
+}
