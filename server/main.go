8ec2619bbc9090e5c16b84d6cfb4d9c070d7b566
alessio <asoseil@gmail.com>
2016-08-24 17:39:27 +0200

    server example updated

--- server/main.go
+++ server/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"io"
-	"net/http"
 	"fmt"
+	"net/http"
+	"strings"
+	"log"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()  // parse arguments, you have to call this by yourself
+	fmt.Println(r.Form)  // print form information in server side
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello!") // send data to client side
 }
 
 func main() {
-	fmt.Println("Server listening on port 0.0.0.0:5000")
-	http.HandleFunc("/", hello)
-	http.ListenAndServe("0.0.0.0:5000", nil)
+	fmt.Println("Server listening on 0.0.0.0:9090")
+	http.HandleFunc("/", sayhelloName) // set router
+	err := http.ListenAndServe("0.0.0.0:9090", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
