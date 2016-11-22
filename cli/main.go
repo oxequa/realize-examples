bd38b39bc1d25169f837ab80bc7ccc97e4edac23
alessio <asoseil@gmail.com>
2016-11-22 18:50:14 +0100

    cli example

---
+++ cli/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/urfave/cli.v2"
+	"os"
+)
+
+func main() {
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "lang",
+				Value: "english",
+				Usage: "language for the greeting",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			name := "Nefertiti"
+			if c.NArg() > 0 {
+				name = c.Args().Get(0)
+			}
+			if c.String("lang") == "spanish" {
+				fmt.Println("Hola", name)
+			} else {
+				fmt.Println("Hello", name)
+			}
+			return nil
+		},
+	}
+
+	app.Run(os.Args)
+}
