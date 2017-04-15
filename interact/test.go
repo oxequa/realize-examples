77c29672cbc1c696e9800556fc2f0e3b1a0c9661
alessio <asoseil@gmail.com>
2017-04-15 20:09:11 +0200

    one config for all projects

---
+++ interact/test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"fmt"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Println(text)
+}
