b2d6485aae24a6955dd7c17831599023a75f2d13
alessio <asoseil@gmail.com>
2017-08-07 23:43:40 +0200

    fresh example

---
+++ fresh/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"github.com/tockins/fresh"
+	"net/http"
+	"os"
+)
+
+type Todo struct {
+	Title string `json:"title"`
+}
+
+func main() {
+	f := fresh.New()
+	f.Config().SetPort(8080)
+	// group
+	g := f.Group("/group/").Before(filter1, filter1).After(filter1, filter1)
+	g.Get("/test/", list).Before(filter2, filter3).After(filter2)
+	g.Get("test/{id}", single)
+	g.Get("test/alpha", single)
+	f.Run()
+}
+
+func list(f fresh.Context) error {
+	data := []Todo{{Title: "Buy milk: list"}}
+	dir, _ := os.Getwd()
+	return f.Response().Download(http.StatusOK, dir+"/listino.pdf")
+	return f.Response().JSON(http.StatusOK, data)
+}
+
+func single(f fresh.Context) error {
+	data := Todo{Title: "Buy milk single"}
+	return errors.New("Error")
+	return f.Response().JSON(http.StatusOK, data)
+}
+
+func filter1(f fresh.Context) error {
+	return nil
+}
+func filter2(f fresh.Context) error {
+	return nil
+}
+func filter3(f fresh.Context) error {
+	return nil
+}
